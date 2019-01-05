425baca7e3df568a09c5481f0dcce064620087f7
William OLAX <olaxwilliam@gmail.com>
2019-01-05 11:59:30 +0100

    Controleurs connexion inscription

--- vues/index.php
+++ vues/index.php
@@ -1,10 +1,18 @@
+<?php
+    function show_error($key)
+    {
+      global $errors; //Récupère la variable $errors dans la portée globale
+      return !empty($errors[$key]) ? '<div class="error">'. $errors[$key] .'</div>' : '';
+    }
+ ?>
+
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-
+<!--TODO : Correction : creerCompte doit prendre un max pour continuer de fonctionner en cas de prenom et nom multiple -->
 <head>
     <meta charset="utf-8">
     <title>Sackado : Partagez sans soucis</title>
-    <link rel="stylesheet" href="./css/master.css">
+    <link rel="stylesheet" href="../vues/css/master.css">
 </head>
 <script type="text/javascript">
   function switchInscConn(){
@@ -34,32 +42,34 @@
         </div>
         <div id="inscription-part" class="is-active  mode-inscription">
             <div class="inscription-header">
-                <a onclick="switchInscConn();">Se connecter</a>
+                <a href="#" onclick="switchInscConn();">Se connecter</a>
                 <a href="#">Télecharger</a>
             </div>
             <div class="inscription-content">
                 <h1 class="titreDroite">Inscrivez-vous <br><em class="optionnel">ou <a href="#" onclick="switchInscConn();">Connectez-vous</a></em></h1>
-                <form id="formInfos" action="index.html" method="post">
-                    <input type="text" name="nom" placeholder="Nom">
+                <form id="formInfos" action="../controleurs/inscription-control.php" method="post">
+                    <input type="text" name="nom" placeholder="Nom" required>
                     <span class="border"></span>
                     <br>
-                    <input type="text" name="prenom" placeholder="Prenom">
+                    <input type="text" name="prenom" placeholder="Prenom" required>
                     <span class="border"></span>
                     <br>
-                    <input type="text" name="mail" placeholder="Adresse email">
+                    <input type="text" name="mail" placeholder="Adresse email" required>
                     <span class="border"></span>
                     <br>
-                    <input type="text" name="pseudo" placeholder="Pseudo">
+                    <input type="text" name="pseudo" placeholder="Pseudo" required>
                     <span class="border"></span>
                     <br>
-                    <input type="text" name="password" placeholder="Mot de passe">
+                    <input type="password" name="password" placeholder="Mot de passe" required>
                     <span class="border"></span>
                     <br>
                     <input type="checkbox" name="conditions" required>
                     <label for="conditions">Acceptez les conditions d'utilisation</label>
                     <br>
 
-                    <span id="erreur"></span>
+                    <span class="erreur"><?php echo(show_error('mail'));?></span>
+                    <span class="erreur"><?php echo(show_error('pseudo'));?></span>
+                    <span class="erreur"><?php echo(show_error('password'));?></span>
                     <br>
                     <input type="submit" name="s'inscrire" value="S'inscrire">
                 </form>
@@ -67,19 +77,19 @@
         </div>
         <div id="connexion-part" class="is-active mode-inscription">
           <div class="inscription-header">
-              <a onclick="switchInscConn();">S'inscrire</a>
+              <a href="#" onclick="switchInscConn();">S'inscrire</a>
               <a href="#">Télecharger</a>
           </div>
           <div class="inscription-content">
               <h1 class="titreDroite">Connectez-vous <br><em class="optionnel">ou <a href="#" onclick="switchInscConn();">Inscrivez-vous</a></em></h1>
-              <form id="formConnexion" action="index.html" method="post">
+              <form id="formConnexion" action="../controleurs/connexion-control.php" method="post">
                   <input type="text" name="pseudo" placeholder="Pseudo">
                   <span class="border"></span>
                   <br>
-                  <input type="text" name="password" placeholder="Mot de passe">
+                  <input type="password" name="password" placeholder="Mot de passe">
                   <span class="border"></span>
                   <br>
-                  <span id="erreur"></span>
+                  <span class="erreur"><?php echo(show_error('connect'));?></span>
                   <br>
                   <input type="submit" name="connecter" value="Se connecter">
               </form>
