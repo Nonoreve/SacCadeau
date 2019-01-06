8c979fdda4451a973a515986cc5f8f6e7ae634ac
Nonoreve <noe.boonstra@gmail.com>
2019-01-06 22:48:31 +0100

    Cadeau creation complete

--- controleurs/creation-cadeau-control.php
+++ controleurs/creation-cadeau-control.php
@@ -1,3 +1,17 @@
 <?php
-
+	session_start();
+	require_once("../data/connect.php");
+	require_once("../data/Cadeau/Cadeau.php");
+	require_once("../data/Utilisateur/Membre.php");
+	$nomCadeau = htmlspecialchars($_POST['NomCadeau']);
+	$descCadeau = htmlspecialchars($_POST['DescCadeau']);
+	$lienCadeau = htmlspecialchars($_POST['LienCadeau']);
+	$imageCadeau = htmlspecialchars($_POST['ImageCadeau']);
+	$nomCadeau = mysqli_real_escape_string($co, $nomCadeau);
+	$descCadeau = mysqli_real_escape_string($co, $descCadeau);
+	$lienCadeau = mysqli_real_escape_string($co, $lienCadeau);
+	$imageCadeau = mysqli_real_escape_string($co, $imageCadeau);
+	$user = new Membre($_SESSION['MembreActif'], $co);
+	Cadeau::creerCadeau($nomCadeau, $descCadeau, $imageCadeau, $lienCadeau, $user -> getLogin(), $co);
+	header("Location: ../vues/pageSackado.php");
 ?>
