780a38c7ad2f2caa8897adc767e9934e820242ed
William OLAX <olaxwilliam@gmail.com>
2019-01-10 13:33:38 +0100

    Systeme Listes Fonctionnel

--- controleurs/inscription-control.php
+++ controleurs/inscription-control.php
@@ -2,7 +2,6 @@
   ini_set('display_errors', 1);
   require_once("../data/connect.php");
   require_once("../data/Utilisateur/Membre.php");
-
   if(!empty($_POST))
   {
     $pseudo = htmlspecialchars($_POST['pseudo']);
@@ -12,7 +11,6 @@
     $pseudo = mysqli_real_escape_string($co, $pseudo);
     $password = mysqli_real_escape_string($co, $password);
     $mail = mysqli_real_escape_string($co, $mail);
-
 
     $result = mysqli_query($co, "SELECT EXISTS(SELECT 1 FROM Membre WHERE LoginMembre = '$pseudo')");
     $resultat = mysqli_fetch_row($result);
@@ -27,22 +25,19 @@
     elseif(empty($password)){
       $errors['password'] = "Le mot de passe ne peut être vide";
     } else {
-
       $nom = mysqli_real_escape_string($co, htmlspecialchars($_POST['nom']));
       $prenom = mysqli_real_escape_string($co, htmlspecialchars($_POST['prenom']));
       $mail = $_POST['mail'];
       $pseudo = $_POST['pseudo'];
       $password = $_POST['password'];
-
       //TODO : MySql : Correction : creerCompte doit prendre un max pour continuer de fonctionner en cas de prenom et nom multiple
       $idMembre = Membre::creerCompte($nom, $prenom, $mail, $pseudo, $password, $co);
       $nouveauMembre = new Membre($idMembre, $co);
       session_start();
       $_SESSION['MembreActif'] = $nouveauMembre -> getId();
-      header("Location: ../vues/primeAbord.html");
+      header("Location: ../vues/primeAbord.php");
     }
-
+    
     include("../vues/index.php");
 }
-
  ?>
